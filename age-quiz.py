7dc2b7a0a70cc20a3b893210dd13211a930f4f22
PhummyM <phummym@gmail.com>
2026-06-26 13:45:19 +0200

    Add files via upload

---
+++ age-quiz.py
@@ -0,0 +1,11 @@
+#Write code to take in a user’s age and store it in an integer variable called age
+age = int(input("Please enter your age: "))
+print(f"Your age is: {age}")
+
+#Assume that the oldest someone can be is 100; if the user enters a higher
+#number, output the message: “Sorry, you're dead.”
+if age > 100:
+    print("Sorry, you're dead.")
+    
+
+
